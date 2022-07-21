20597adda089e085338c8e3e89b8e61f39a9b75e
DebuggerOR <foglerori@gmail.com>
2022-07-21 06:25:43 +0300

    .

--- environment.py
+++ environment.py
@@ -1,13 +1,11 @@
 import time
-from random import randint, uniform, seed
+from random import randint, seed
 from typing import List, Tuple, Dict, Union
 
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import networkx as nx
-from shapely.geometry import Point, Polygon
-
-from prm import PRM
+from shapely.geometry import Point, Polygon, LineString
 
 
 class Environment:
@@ -16,33 +14,64 @@ class Environment:
         self._seed_value = seed_value
         seed(self._seed_value)
 
+        # dimensions
         self._x_range = x_range
         self._y_range = y_range
 
+        # entities and targets
         self._no_entrances = self._create_entities(num_no_entrances, (self._x_range / 100, self._x_range / 10))
-        self._threats = self._create_entities(num_threats, (self._x_range / 10, self._x_range / 5))
+        self._no_entrances_shapes = [ne['shape'] for ne in self._no_entrances]
 
-        self._create_targets(num_targets)
+        self._threats = self._create_entities(num_threats, (self._x_range / 10, self._x_range / 5))
+        self._threats_shapes = [threat['shape'] for threat in self._threats]
+
+        self._targets = self._create_targets(num_targets)
+        self._targets_names = [f'({target.x},{target.y})' for target in self._targets]
 
     @property
-    def x_range(self):
+    def x_range(self) -> int:
         return self._x_range
 
     @property
-    def y_range(self):
+    def y_range(self) -> int:
         return self._y_range
 
     @property
-    def no_entrances(self):
+    def no_entrances(self) -> List[Dict[str, Union[Point, float, Polygon]]]:
         return self._no_entrances
 
     @property
-    def threats(self):
+    def no_entrances_shapes(self) -> List[Polygon]:
+        return self._no_entrances_shapes
+
+    @property
+    def threats(self) -> List[Dict[str, Union[Point, float, Polygon]]]:
         return self._threats
 
     @property
-    def targets(self):
+    def threats_shapes(self) -> List[Polygon]:
+        return self._threats_shapes
+
+    @property
+    def targets(self) -> List[Point]:
         return self._targets
+
+    @property
+    def targets_names(self) -> List[str]:
+        return self._targets_names
+
+    def is_legal_point(self, point: Point) -> bool:
+        for obstacle in self._no_entrances:
+            if obstacle['shape'].contains(point):
+                return False
+        return True
+
+    def is_legal_edge(self, p1: Point, p2: Point) -> bool:
+        line = LineString([p1, p2])
+        for ne in self._no_entrances_shapes:
+            if line.intersects(ne):
+                return False
+        return True
 
     def _create_entities(self, num_entities: int, radius_range: Tuple[float, float]) \
             -> List[Dict[str, Union[Point, float, Polygon]]]:
@@ -54,19 +83,20 @@ class Environment:
         return [{'center': point, 'radius': radius, 'shape': point.buffer(radius)}
                 for point, radius in zip(entities_points, rads)]
 
-    def _target_is_in_obstacle(self, target: Point) -> bool:
-        for obstacle in self._no_entrances:
-            if obstacle['shape'].contains(target):
-                return True
-        return False
-
-    def _create_targets(self, num_targets: int) -> None:
+    def _create_targets(self, num_targets: int) -> List[Point]:
         # create targets out of no entrance zones
-        self._targets = []
-        while len(self._targets) < num_targets:
+        targets = []
+        while len(targets) < num_targets:
             potential_target = Point(randint(0, self._x_range), randint(0, self._y_range))
-            if not self._target_is_in_obstacle(potential_target):
-                self._targets.append(potential_target)
+            if self.is_legal_point(potential_target):
+                targets.append(potential_target)
+        return targets
+
+    def sample(self) -> Point:
+        rand_point = Point(randint(0, self._x_range), randint(0, self._y_range))
+        while not self.is_legal_point(rand_point):
+            rand_point = Point(randint(0, self._x_range), randint(0, self._y_range))
+        return rand_point
 
     def plot(self) -> None:
         plt.figure(figsize=(10, 10))
@@ -94,16 +124,5 @@ class Environment:
         # plot legend
         plt.legend(handles=[mpatches.Patch(color='gold', label=f'{len(self._targets)} targets'),
                             mpatches.Patch(color='blue', label=f'{len(self._no_entrances)} no entrance zones'),
-                            mpatches.Patch(color='red', label=f'{len(self._threats)} threats')], ncol=3, loc='upper center')
-
-
-if __name__ == '__main__':
-    # create environment
-    env = Environment()
-    env.plot()
-
-    # create roadmap
-    prm = PRM(env.x_range, env.y_range, [ne['shape'] for ne in env.no_entrances], env.targets)
-    prm.extend_iterations(100)
-    prm.plot()
-    plt.show()
+                            mpatches.Patch(color='red', label=f'{len(self._threats)} threats')], ncol=3,
+                   loc='upper center')
